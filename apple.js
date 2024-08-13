a2c43eddad7d2fc63602346a1811fc8d12678055
Mustaafa-h <86521814+Mustaafa-h@users.noreply.github.com>
2024-08-13 22:16:10 +0300

    Add files via upload

---
+++ apple.js
@@ -0,0 +1,12 @@
+let img = document.querySelector('.image');
+let con = document.querySelector('.container')
+
+function phones(phon) 
+{
+    img.src = phon;
+}
+
+function color(color)
+{
+    con.style.background = color;
+}
